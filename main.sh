530aa5db71d15ca1b6031f0ef4c1cc00d520c2b5
Sun Tan <sutan@redhat.com>
2021-05-01 18:05:22 +0000

    using pushover.net to get notify
    Signed-off-by: Sun Tan <sutan@redhat.com>

--- main.sh
+++ main.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-curl 'https://tennis.paris.fr/tennis/jsp/site/Portal.jsp?page=recherche&action=ajax_disponibilite_map' \
-  --data-raw 'hourRange=8-22&when=03%2F05%2F2021&selCoating%5B%5D=96&selCoating%5B%5D=2095&selCoating%5B%5D=94&selCoating%5B%5D=1324&selCoating%5B%5D=2016&selCoating%5B%5D=92&selInOut%5B%5D=V&selInOut%5B%5D=F' \
-| jq '.features[] | select(.properties.available)'
 
+
+json=$(curl 'https://tennis.paris.fr/tennis/jsp/site/Portal.jsp?page=recherche&action=ajax_disponibilite_map' \
+  --data-raw 'hourRange=8-22&when=02%2F05%2F2021&selCoating%5B%5D=96&selCoating%5B%5D=2095&selCoating%5B%5D=94&selCoating%5B%5D=1324&selCoating%5B%5D=2016&selCoating%5B%5D=92&selInOut%5B%5D=V&selInOut%5B%5D=F' \
+| jq '.features[] | select(.properties.available) | .properties.general | {nom: ."_nomSrtm", id: ."_id"}')
+
+if [[ "$json" != "$(cat /tmp/tennis.json)" ]]
+then
+  echo "$json" > /tmp/tennis.json
+  echo "####################################################\n########### New value: \n$json"
+  curl -s \
+  --form-string "token=$PUSHOVER_APP_TOKEN" \
+  --form-string "user=$PUSHOVER_USER_KEY" \
+  --form-string "message=Nouveau tennis dispo ! \n$json" \
+  https://api.pushover.net/1/messages.json
+fi
