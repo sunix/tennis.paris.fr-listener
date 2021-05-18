1c4afab3b47062fd6e8201fe0a4dabe0a062cf1d
Sun Seng David TAN <sutan@redhat.com>
2021-05-18 09:42:17 +0200

    feat(filtering): adding filtering by court name
    Signed-off-by: Sun Seng David TAN <sutan@redhat.com>

--- main.sh
+++ main.sh
@@ -3,8 +3,8 @@
 
 
 json=$(curl 'https://tennis.paris.fr/tennis/jsp/site/Portal.jsp?page=recherche&action=ajax_disponibilite_map' \
-  --data-raw 'hourRange=8-22&when=02%2F05%2F2021&selCoating%5B%5D=96&selCoating%5B%5D=2095&selCoating%5B%5D=94&selCoating%5B%5D=1324&selCoating%5B%5D=2016&selCoating%5B%5D=92&selInOut%5B%5D=V&selInOut%5B%5D=F' \
-| jq '.features[] | select(.properties.available) | .properties.general | {nom: ."_nomSrtm", id: ."_id"}')
+  --data-raw 'hourRange=12-14&when=21%2F05%2F2021&selCoating%5B%5D=96&selCoating%5B%5D=2095&selCoating%5B%5D=94&selCoating%5B%5D=1324&selCoating%5B%5D=2016&selCoating%5B%5D=92&selInOut%5B%5D=V&selInOut%5B%5D=F' \
+| jq '.features[] | select(.properties.available and (.properties.general._nomSrtm=="Philippe Auguste" or .properties.general._nomSrtm=="Candie" or .properties.general._nomSrtm=="Thiéré")) | .properties.general | {nom: ."_nomSrtm", id: ."_id"}')
 
 if [[ "$json" != "$(cat /tmp/tennis.json)" ]]
 then
